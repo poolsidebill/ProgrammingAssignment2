6289b65cea3825640adef53b844e349e0119c9d8
Bill Case <bcase59@gmail.com>
2014-05-25 20:05:24 -0500

    Final touches for both functions. Homework ready for eval by my peers

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-## R Programming Class assignment to create some functions
+## R Programming Class assignment to create some functions:
 ## to create a matrix, and to cache the matrix's inverse.
 ## These functions can then be used in further activities
 ## that the class may dream up.
@@ -6,9 +6,8 @@
 
 ## I provide mechanisms to set and get a matrix, and
 ## I can set and get the inverse of this matrix
-
 makeCacheMatrix <- function(storedMatrix = matrix()) {
-  inverseMatrix <- NULL
+  inverseMatrix <- NULL ## cached matrix inverse of this object
 
   set <- function(newMatrix) {
     storedMatrix <<- newMatrix   # set the matrix value
@@ -26,8 +25,29 @@ makeCacheMatrix <- function(storedMatrix = matrix()) {
 } # end of makeCacheMatrix
 
 
-## Write a short comment describing this function
-
+## I return the inverse matrix of the matrix passed in
+## If a cached inverse matrix is in this object, return it
+## Otherwise, create the inverse, cache it in object, and return it
+## I expect the first arg in this function to be a special object
+## created from the makeCacheMatrix function above.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  matrixInverse <- x$getInverse()
+  if(!is.null(matrixInverse) {
+    ## I have found the cached inverse matrix of this object
+    ## and will return it
+    message("getting cached inverse matrix of this object")
+    return(matrixInverse)
+  }
+  
+  ## No previous cached matrix inverse detected
+  ## Create a new CacheMatrix ann return its inverse
+  newMatrix <- x$get()
+  matrixInverse <- solve(x)
+
+  ## save the inverse in the object so we don't need to
+  ## recaculate it next time
+  x$setInverse(matrixInverse)
+
+  ## return the inverse to caller
+  matrixInverse
 }
